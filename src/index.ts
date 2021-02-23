b9f3292888d29c9c37d98508792cf2d1b5b0b7cb
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-02-23 15:17:42 -0500

    feat: added graphql and server port

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
+import "reflect-metadata";
 //to compile ts files in node -> use ts-node
-import { MikroORM } from "@mikro-orm/core"
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-// import { Post } from "./entities/Post";
-import microConfig from "./mikro-orm.config"
+import microConfig from "./mikro-orm.config";
+
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'; //for GraphQL endpoints
+import { buildSchema } from "type-graphql"; //create graphql schema
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
 const main = async () => {
     //handle database transactions
     const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post); // roughly equivalent to await orm.em.nativeInsert(Post, {title:'my first post'})
-    // const posts = await orm.em.find(Post, {})
-    // console.log(posts)
+    await orm.getMigrator().up(); //runs when server restarts, don't rerun old migrations
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers:[UserResolver, HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('App is listening on port 4000.');
+    })
 }
 
 main().catch((error) => {
