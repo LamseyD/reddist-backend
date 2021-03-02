88a7324da56bd07162c8110d3c54ffd250f729c9
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-03-02 16:50:39 -0500

    feat(session): added session cookies to store login info

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 
-import express from 'express'
+import express from 'express';
+import redis from 'redis';
+// import Redis from "ioredis";
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 import { ApolloServer } from 'apollo-server-express'; //for GraphQL endpoints
 import { buildSchema } from "type-graphql"; //create graphql schema
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
+
+
 
 const main = async () => {
     //handle database transactions
@@ -18,12 +25,36 @@ const main = async () => {
 
     const app = express();
 
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient({host: "127.0.0.1", port: 6379});
+
+    //run session middleware before apollo middleware because you want sessions inside apollo
+    app.use(
+        session({
+            name: 'qid', //cookie name
+            //when storing data in redis, ttl means how long data last in redis. if user does an action, ping 
+            //redis and extend the data ttl. if disable touch/ttl makes redis data live forever, however this shouldn't be in production
+            store: new RedisStore({ client: redisClient, disableTouch: true}), //tell express we're using redis
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //cookie last for 10 years?
+                httpOnly: true, //security reason, can't access the cookie on front end
+                sameSite: 'lax', //protecting against csrf
+                secure: !__prod__//cookie only works in https (production)
+            },
+            saveUninitialized: false,
+            secret: 'keyboard cat', //cookie secret
+            resave: false,
+        })
+    );
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers:[UserResolver, HelloResolver, PostResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({req, res}): MyContext => {
+            return ({ em: orm.em, req, res });
+        } //access req, res context in apollo server with express
     })
 
     apolloServer.applyMiddleware({ app })
@@ -34,6 +65,6 @@ const main = async () => {
 }
 
 main().catch((error) => {
-    console.error(error);
+    console.error(`error: ${error}`);
 });
 //returns a promise
