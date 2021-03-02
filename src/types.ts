e5fd2079bd07a3a23afcf15e2ed14983d353f47a
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-03-02 18:35:01 -0500

    refactor: updated MyContext types

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,6 @@ import { Session } from 'express-session';
 
 export type MyContext = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-    req: Request & { session: Session }; // \? means possible undefined & joins 2 types together
+    req: Request & { session: Session & { userId: number}}; // \? means possible undefined & joins 2 types together
     res: Response;
 }
