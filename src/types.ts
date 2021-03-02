9d3ee4257e6b038c231ce962db3af1336581a167
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-03-02 16:51:09 -0500

    feat(types): added specific types for req and session

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
 import { Connection, IDatabaseDriver, EntityManager } from "@mikro-orm/core";
-
+import { Request, Response } from 'express';
+import { Session } from 'express-session';
 
 export type MyContext = {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+    req: Request & { session: Session }; // \? means possible undefined & joins 2 types together
+    res: Response;
 }
